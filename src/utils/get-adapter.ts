f6f8b920516240fa52d65af4f8041a8a8463e6fb
mrgnhnt96 <mrgnhnt96@gmail.com>
2020-04-24 20:31:24 -0700

    added adapter to adapter name

--- src/utils/get-adapter.ts
+++ src/utils/get-adapter.ts
@@ -1,3 +1,3 @@
-export function getAdapter(adapterName: string): string {
-  return `Hive.registerAdapter(${adapterName}());`;
+export function getAdapter(className: string): string {
+  return `Hive.registerAdapter(${className}Adapter());`;
 }
